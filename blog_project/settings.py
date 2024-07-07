deb6b18e50e0e13756aa890c4d89b9847953765c
Sangameeee <137791865+Sangameeee@users.noreply.github.com>
2024-07-07 19:06:20 +0545

    set reset
    added password set reset option

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -132,3 +135,12 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 LOGIN_REDIRECT_URL  = 'home'
 LOGIN_URL = 'login'
+
+load_dotenv()
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('GMAIL_ID')
+EMAIL_HOST_PASSWORD = os.getenv('GMAIL_PASS')
+
